fc1e7d4546ad0a11eed00375108e724fb3f5d0c7
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-30 13:40:34 +0300

    User model added

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+    },
+    profileImg: String,
+
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, 'Too Short Password'],
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
