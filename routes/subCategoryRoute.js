c0a95c4cd6a6a1166695f15bb88ca7bbfebf9bc9
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 12:09:57 +0300

    added Authorization to subcategory Route

--- routes/subCategoryRoute.js
+++ routes/subCategoryRoute.js
@@ -17,17 +17,35 @@ const {
   deleteSubCategoryValidator,
 } = require('../utils/validators/subCategoryValidator');
 
+const { protect, allowedTo } = require('../services/authService');
+
 //@desc allow access to the category
 const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
-  .post(setCategoryIdToBody, createSubCategoryValidator, createSubCategory)
+  .post(
+    protect,
+    allowedTo('admin', 'manager'),
+    setCategoryIdToBody,
+    createSubCategoryValidator,
+    createSubCategory
+  )
   .get(createFilterObj, getSubCategories);
 router
   .route('/:id')
   .get(getSubCategoryValidator, getSubCategory)
-  .put(updateSubCategoryValidator, updateSubCategory)
-  .delete(deleteSubCategoryValidator, deleteSubCategory);
+  .put(
+    protect,
+    allowedTo('admin', 'manager'),
+    updateSubCategoryValidator,
+    updateSubCategory
+  )
+  .delete(
+    protect,
+    allowedTo('admin'),
+    deleteSubCategoryValidator,
+    deleteSubCategory
+  );
 
 module.exports = router;
