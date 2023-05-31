dee8b758ab236f07c69545524d366d0d946d8956
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 16:11:28 +0300

    verify reset code added to authRoute

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const { signup, login, forgotPassword } = require('../services/authService');
+const {
+  signup,
+  login,
+  forgotPassword,
+  verifyPassResetCode,
+} = require('../services/authService');
 
 const {
   signupValidator,
@@ -13,5 +18,6 @@ router.route('/signup').post(signupValidator, signup);
 
 router.route('/login').post(loginValidator, login);
 router.route('/forgotPassword').post(forgotPasswordValidator, forgotPassword);
+router.route('/verifyResetCode').post(verifyPassResetCode);
 
 module.exports = router;
