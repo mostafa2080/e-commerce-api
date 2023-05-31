100f455664d1f11682db24c25a7edae4c7f11012
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 15:51:32 +0300

    auth route modified to include the forgot password route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
-const { signup, login } = require('../services/authService');
+const { signup, login, forgotPassword } = require('../services/authService');
 
 const {
   signupValidator,
   loginValidator,
+  forgotPasswordValidator,
 } = require('../utils/validators/authValidator');
 
 router.route('/signup').post(signupValidator, signup);
 
 router.route('/login').post(loginValidator, login);
+router.route('/forgotPassword').post(forgotPasswordValidator, forgotPassword);
 
 module.exports = router;
