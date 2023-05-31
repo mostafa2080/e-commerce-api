9159068bec96670c5f93e261be2f2c03471d4222
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 08:29:04 +0300

    validation layer for login added

--- utils/validators/authValidator.js
+++ utils/validators/authValidator.js
@@ -1,5 +1,4 @@
-const bcrypt = require('bcryptjs');
-const { check, body } = require('express-validator');
+const { check } = require('express-validator');
 const slugify = require('slugify');
 const validatorMiddleware = require('../../middlewares/validatorMiddleware');
 const User = require('../../models/userModel');
@@ -40,5 +39,28 @@ exports.signupValidator = [
     .notEmpty()
     .withMessage('password confirmation required'),
 
+  validatorMiddleware,
+];
+
+exports.loginValidator = [
+  check('name')
+    .notEmpty()
+    .isLength({ min: 3 })
+    .withMessage('Too Short User Name')
+
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  check('email')
+    .notEmpty()
+    .isEmail()
+    .withMessage('Please enter a valid email address'),
+
+  check('password')
+    .notEmpty()
+    .withMessage('password required')
+    .isLength({ min: 6 })
+    .withMessage('password must be at least 6 characters'),
   validatorMiddleware,
 ];
