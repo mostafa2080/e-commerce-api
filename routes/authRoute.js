b93a9045f3e925bcf569b9c77a3d2d3a9a471fcd
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 09:37:39 +0300

    added login route to auth route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-const { signup } = require('../services/authService');
+const { signup, login } = require('../services/authService');
 
-const { signupValidator } = require('../utils/validators/authValidator');
+const {
+  signupValidator,
+  loginValidator,
+} = require('../utils/validators/authValidator');
 
 router.route('/signup').post(signupValidator, signup);
+
+router.route('/login').post(loginValidator, login);
 
 module.exports = router;
