9922f6a9c697032433dbf6ddb21ecd0f25643343
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 09:34:47 +0300

    Modifying JwtError for production

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,4 +1,4 @@
-
+const ApiError = require('../utils/apiError');
 
 const sendErrForDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -15,12 +15,24 @@ const sendErrForProd = (err, res) => {
   });
 };
 
+const handleJwtInvalidSignature = () =>
+  new ApiError('Invalid Token Please Login Again...', 401);
+const handleJwtExpired = () =>
+  new ApiError('Token Expired Please Login Again...', 401);
+
 const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'development') {
     sendErrForDev(err, res);
   } else {
+    if (err.name === 'JsonWebTokenError') {
+      err = handleJwtInvalidSignature();
+    }
+    if (err.name === 'TokenExpiredError') {
+      err = handleJwtExpired();
+    }
+
     sendErrForProd(err, res);
   }
 };
