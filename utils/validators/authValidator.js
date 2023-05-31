07b44ba939b44935b69d18b6fda097d22a236483
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 07:44:06 +0300

    singup validation layer added to authValidator

---
+++ utils/validators/authValidator.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const { check, body } = require('express-validator');
+const slugify = require('slugify');
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
+const User = require('../../models/userModel');
+
+exports.signupValidator = [
+  check('name')
+    .notEmpty()
+    .isLength({ min: 3 })
+    .withMessage('Too Short User Name')
+
+    .custom((val, { req }) => {
+      req.body.slug = slugify(val);
+      return true;
+    }),
+  check('email')
+    .notEmpty()
+    .isEmail()
+    .withMessage('Please enter a valid email address')
+    .custom((val) =>
+      User.findOne({ email: val }).then((user) => {
+        if (user) {
+          return Promise.reject(new Error('Email already exists'));
+        }
+      })
+    ),
+  check('password')
+    .notEmpty()
+    .withMessage('password required')
+    .isLength({ min: 6 })
+    .withMessage('password must be at least 6 characters')
+    .custom((password, { req }) => {
+      if (password !== req.body.passwordConfirmation) {
+        throw new Error('password does not match');
+      }
+      return true;
+    }),
+  check('passwordConfirmation')
+    .notEmpty()
+    .withMessage('password confirmation required'),
+
+  validatorMiddleware,
+];
