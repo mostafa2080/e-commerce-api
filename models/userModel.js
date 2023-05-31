5f2490ba0ca40c58de5660d8efb4282712134e31
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 05:50:36 +0300

    hashing password for user added

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -43,6 +44,13 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
+const User = mongoose.model('User', userSchema);
 module.exports = User;
