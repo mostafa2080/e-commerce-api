91e4fabc08921f1fbf76c7e9f250f54053f4224d
mostafa2080 <mostafashaaban2080@gmail.com>
2023-07-01 17:17:43 +0300

    adding the order model and create cash order handler

---
+++ services/orderService.js
@@ -0,0 +1,57 @@
+// const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const asyncHandler = require('express-async-handler');
+const factory = require('./handlersFactory');
+const ApiError = require('../utils/apiError');
+
+const User = require('../models/userModel');
+const Product = require('../models/productModel');
+const Cart = require('../models/cartModel');
+const Order = require('../models/orderModel');
+
+// @desc    create cash order
+// @route   POST /api/v1/orders/cartId
+// @access  Protected/User
+exports.createCashOrder = asyncHandler(async (req, res, next) => {
+  // app settings
+  const taxPrice = 0;
+  const shippingPrice = 0;
+
+  // 1) Get cart depend on cartId
+  const cart = await Cart.findById(req.params.cartId);
+  if (!cart) {
+    return next(
+      new ApiError(`There is no such cart with id ${req.params.cartId}`, 404)
+    );
+  }
+
+  // 2) Get order price depend on cart price "Check if coupon apply"
+  const cartPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount
+    : cart.totalCartPrice;
+
+  const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+
+  // 3) Create order with default paymentMethodType cash
+  const order = await Order.create({
+    user: req.user._id,
+    cartItems: cart.cartItems,
+    shippingAddress: req.body.shippingAddress,
+    totalOrderPrice,
+  });
+
+  // 4) After creating order, decrement product quantity, increment product sold
+  if (order) {
+    const bulkOption = cart.cartItems.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product },
+        update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+      },
+    }));
+    await Product.bulkWrite(bulkOption, {});
+
+    // 5) Clear cart depend on cartId
+    await Cart.findByIdAndDelete(req.params.cartId);
+  }
+
+  res.status(201).json({ status: 'success', data: order });
+});
