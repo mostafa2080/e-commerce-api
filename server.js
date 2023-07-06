bfc6d74f6ebce63edd5b49bf4b50bc05c47cba4b
mostafa2080 <mostafashaaban2080@gmail.com>
2023-07-07 01:02:32 +0300

    limiting data recived to 20kb request size limit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post(
 dbconnection();
 
 //Middleware
-app.use(express.json());
+app.use(express.json({ limit: '20kb' }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
